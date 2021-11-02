816421ee9ae3c949ba6ff113a61163041ab61848
awaisspk <iamawaisiqball@gmail.com>
2021-11-02 20:00:21 +0500

    Feat: Site layout

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,12 @@
+import {Footer} from '@components/Footer';
+import {Header} from '@components/Header';
+
+export const Layout: React.FC = ({children}) => {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  );
+};
