ea5b4c7c32445011ac873df3f00ba0f547ff082c
awaisspk <iamawaisiqball@gmail.com>
2021-11-03 14:53:27 +0500

    Feat: Site icons

--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -2,3 +2,6 @@ export {Collaboration} from './Collaboration';
 export {Email} from './Email';
 export {Arrow} from './Arrow';
 export {Logo} from './Logo';
+export {AccessAnywhere} from './accessAnywhere';
+export {Store} from './Store';
+export {Security} from './Security';
